4d8bd4b9884518288d8f437aebf9024aa2a242b2
zaoky <proxtos@gmail.com>
2016-07-27 16:25:51 -0600

    Updating ListView

--- SqliteSample/app/page/main/main-view-model.ts
+++ SqliteSample/app/page/main/main-view-model.ts
@@ -3,6 +3,8 @@ import { ObservableArray } from 'data/observable-array';
 import * as Database from '../../services/DatabaseService';
 import { PersonModel } from '../../services/person-model';
 import { ItemEventData } from 'ui/list-view';
+import { ListView } from 'ui/list-view';
+import * as lodash from 'lodash';
 
 export class MainViewModel extends Observable {
     Person: PersonModel;
@@ -11,10 +13,16 @@ export class MainViewModel extends Observable {
         super();
         this.Person = new PersonModel();
     }
-    
+
+    onItemTap(args: ItemEventData){
+        this.Person = this.PersonList.getItem(args.index);
+         this.set("Person", this.Person);
+    }
+
     insert() {
         Database.insert(this.Person).then(id => {
             console.log("insert success", id);
+            this.select();
         }, error => {
             console.log("insert error", error);
         });
@@ -22,23 +30,25 @@ export class MainViewModel extends Observable {
 
     select() {
         Database.select(this.Person.TableName).then(rows => {
-            let tempList = new ObservableArray<PersonModel>();
             for (var i = 0; i < rows.length; i++) {
                 let model = new PersonModel();
                 model.ID = rows[i]["ID"];
                 model.firstname = rows[i]["firstname"];
                 model.lastname = rows[i]["lastname"];
                 model.CreatedAt = rows[i]["CreatedAt"];
-                tempList.push(model);
+                if ((this.PersonList.filter(person => person.ID == model.ID)).length === 0) {
+                    this.PersonList.push(model);
+                }
                 console.log("rows ", rows.length);
             }
-            this.PersonList = tempList;
+
+            this.set("PersonList", this.PersonList);
         }, error => {
             console.log("select error", error);
         });
     }
 
-    remove(){
+    remove() {
         Database.remove(this.Person.TableName, `firstname = '${this.Person.firstname}'`).then(id => {
             console.log("remove success", id);
         }, error => {
@@ -46,10 +56,10 @@ export class MainViewModel extends Observable {
         });
     }
 
-    update(){
-        Database.update(this.Person, `where firstname = '${this.Person.firstname}'`).then(id =>{
+    update() {
+        Database.update(this.Person, `where firstname = '${this.Person.firstname}'`).then(id => {
             console.log("update success", id);
-        }, error =>{
+        }, error => {
             console.log("update error", error);
         });
     }
